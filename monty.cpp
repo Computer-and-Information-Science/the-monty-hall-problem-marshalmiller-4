28a0536cb66eee8f7781b2407d07bbc516906094
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2024-01-23 12:23:11 -0500

    Update monty.cpp

--- monty.cpp
+++ monty.cpp
@@ -1,1 +1,36 @@
+#include "monty.h"
+#include <iostream>
+#include <cstdlib>
+#incldue <ctime>
+using namespace std;
 
+bool monty(char strategy) {
+  int car = rand() %3;
+  int pick = rand() %3;
+  if (strategy == 'r') 
+    if (rand() %2==0)
+      strategy = 'k';
+    else 
+      strategy = 's';
+}
+switch (strategy){
+  case 'k':
+  if (pick!=car)
+    return true;
+  break;
+  case 's':
+  if (pick!=car)
+    return true;
+}
+return false;
+}
+
+}
+int monty(char strategy, int time) {
+int count=0;
+  for int =0; i < time; i++) {
+    if (monty(strategy))
+      count++;
+  }
+return count;
+}
